0a3dd1095cb66458a3fb2e4190d7608e6e74394e
sabuto <robe_dunne@hotmail.com>
2023-03-07 20:18:02 +0000

    chore: move folders

--- packages/pieces/trello/src/index.ts
+++ packages/pieces/trello/src/index.ts
@@ -1,20 +1,14 @@
 import { createPiece } from '@activepieces/framework';
 import packageJson from '../package.json';
 import { getCard } from './lib/actions/get-card';
-import { newCard } from './lib/actions/triggers/new-card';
+import { newCard } from './lib/triggers/new-card';
 
 export const trello = createPiece({
   name: 'trello',
   displayName: 'trello',
   logoUrl: 'https://cdn.activepieces.com/pieces/trello.png',
   version: packageJson.version,
-  authors: [
-    'ShayPunter','Sabuto'
-  ],
-  actions: [
-    getCard
-  ],
-  triggers: [
-    newCard
-  ],
+  authors: ['ShayPunter', 'Sabuto'],
+  actions: [getCard],
+  triggers: [newCard],
 });
